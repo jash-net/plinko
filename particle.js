0eea52eb00a69a61b16f8b59ec56fa86e124609f
jash-net <77054377+jash-net@users.noreply.github.com>
2021-05-28 12:06:21 +0530

    Add files via upload

---
+++ particle.js
@@ -0,0 +1,26 @@
+class Particle{
+    constructor(x,y,r) {
+      var options = {
+        restitution : 0.2
+      }
+      this.x = x
+      this.y = y
+      this.r = r 
+      this.body = Bodies.circle(x,y,r,options)
+      this.color = color (random(0,255),random(0,255),random(0,255))
+      World.add(world, this.body)
+    }
+display(){
+    var pos = this.body.position
+    
+    push();
+    translate(pos.x, pos.y);
+    rotate(this.body.angle);
+    
+   fill(this.color);
+    ellipseMode(RADIUS);
+    ellipse(0,0,this.r,this.r);
+    pop();
+}
+
+}
